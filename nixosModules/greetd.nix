34abe19fc2df44b455bf91faadec642666715a86
Sammy Al Hashemi <sammy@salh.xyz>
2026-01-22 01:32:49 -0500

    fix(greetd): remove broken background image from tuigreet

--- nixosModules/greetd.nix
+++ nixosModules/greetd.nix
@@ -11,7 +11,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = lib.mkForce "${pkgs.greetd.tuigreet}/bin/tuigreet --time --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session --bg ${../common/assets/BLACK_VII_desktop.jpg}";
+          command = lib.mkForce "${pkgs.greetd.tuigreet}/bin/tuigreet --time --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
           user = "greeter";
         };
       };
