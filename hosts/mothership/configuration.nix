bfe0c1477568a3e0c25e55c02cf52e639af5a832
Sammyalhashe <sammy@salh.xyz>
2026-08-07 22:17:42 -0400

    [constants] adding mothership key

--- hosts/mothership/configuration.nix
+++ hosts/mothership/configuration.nix
@@ -225,6 +225,7 @@ in
       "input"
     ];
     openssh.authorizedKeys.keys = [
+      config.hostKeys.homebase
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPx5JBI3FNtugjdVeb1Gg4lUEJvGa/eiZ6rnsIN/oC3f sammy@salh.xyz"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2hNthWiWeNoxH848/Vhdkc8jWNJw7690ZNAh8RVE9d sammy@salh.xyz"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZKrkpzxAf0u3+fn59xouUtVHtklRuGwCwfPpR0Y8nc sammy.alhashemi@mail.utoronto.ca"
--- modules/constants.nix
+++ modules/constants.nix
@@ -8,6 +8,7 @@ in
     type = types.attrsOf types.str;
     default = {
       homebase = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2hNthWiWeNoxH848/Vhdkc8jWNJw7690ZNAh8RVE9d sammy@salh.xyz";
+      mothership = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPx5JBI3FNtugjdVeb1Gg4lUEJvGa/eiZ6rnsIN/oC3f sammy@salh.xyz";
     };
     description = "Per-host SSH public keys.";
   };
