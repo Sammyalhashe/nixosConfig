59f703b9606902cdc6f618c13598e2852e88c2a5
Sammyalhashe <sammy@salh.xyz>
2026-08-12 13:40:54 -0400

    [constants] Adding oldboy ssh key.

--- modules/constants.nix
+++ modules/constants.nix
@@ -10,6 +10,7 @@ in
       homebase = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2hNthWiWeNoxH848/Vhdkc8jWNJw7690ZNAh8RVE9d sammy@salh.xyz";
       mothership = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPx5JBI3FNtugjdVeb1Gg4lUEJvGa/eiZ6rnsIN/oC3f sammy@salh.xyz";
       starship = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUptk+nhbHYTfUJvGT3/X4vkKWRotT5ckw8BiQuADml sammy@salh.xyz";
+      oldboy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvEm47CyJZ/UzNn3uySsiOENhFjZapoaeOmpAxWSrPO sammy@salh.xyz";
     };
     description = "Per-host SSH public keys.";
   };
